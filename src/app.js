1c5dca3ee76180e845094e1b725b2d7a6afe5d37
Mark Lyck <mark.lyck@gmail.com>
2016-09-12 12:57:40 -0500

    Addeds styles

--- src/app.js
+++ src/app.js
@@ -3,7 +3,13 @@
 var express = require('express'),
       posts = require('./mock/posts.json');
 
+var postsList = Object.keys(posts).map(function(value) {
+  return posts[value];
+});
+
 var app = express();
+
+app.use('/static', express.static(__dirname + '/public'))
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
@@ -16,7 +22,7 @@ app.get('/blog/:title?', function(req, res) {
   var title = req.params.title;
   if (title === undefined) {
     res.status(503);
-    res.send('This page is under construction!');
+    res.render('blog', { posts: postsList})
   } else {
     var post = posts[title] || {};
     res.render('post', { post: post});
